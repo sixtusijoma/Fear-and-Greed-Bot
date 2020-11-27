d2c6136630d8cb527f5b71a6ecb4e0207e9429ee
Sixtus Ijoma <69412018+sixtusijoma@users.noreply.github.com>
2020-11-27 18:36:18 -0500

    Update FGIBot1.js
    updated header comments

--- FGIBot1.js
+++ FGIBot1.js
@@ -1,9 +1,15 @@
 
-/* 
+/*
+ The Cryptocurrency Fear and Greed Index is a number that displays the current 
+ sentiment towards Bitcoin and other cryptocurrencies. The number ranges from 0-100,
+ as 0 being extreme fear and 100 being extreme greed. Extreme fear can be a sign that
+ investors are too worried. That could be a buying opportunity. When Investors are
+ getting too greedy, that means the market is due for a correction. 
+
  The website "https://alternative.me/crypto/fear-and-greed-index/" calculates and posts the 
  Fear and Greed Index for the cryptocurrency market once daily.
  This bot finds the Fear and Greed Index information from the site 
- and tweets them out (@FearAndGreedBot) everyday, a couple hours after they are updated.
+ and tweets them out everyday, a couple hours after they are updated.
  "FG" is an abbreviation for Fear and Greed in variable names.
 */
 
